0c7ee98ffccff42f5eda39f2b64206c5eeea308c
arozovyk <artemiy.rozovyk@ocamlpro.com>
2023-10-24 12:04:51 +0200

    add some types and style

--- src/index.js
+++ src/index.js
@@ -17,140 +17,319 @@ Blockly.common.defineBlocks(blocks);
 Object.assign(javascriptGenerator.forBlock, forBlock);
 
 // Set up UI elements and inject Blockly
-const codeDiv = document.getElementById("generatedCode").firstChild;
-const outputDiv = document.getElementById("output");
 const blocklyDiv = document.getElementById("blocklyDiv");
 
-const toolbox = ` <xml xmlns="https://developers.google.com/blockly/xml" id="toolbox" style="display: none">
-
-<category name="Operation">
-  <block type="operation">
-    <field name="sur_assiette">les_productions_du_chameau</field>
-    <value name="operation">
-      <block type="text">
-        <field name="TEXT"></field>
-      </block>
-    </value>
-  </block>
-  <block type="operation_local_pool_decl">  </block>
-  <block type="evenement_atteint">
-    <value name="evenement_atteint">
-      <block type="quand">
-        
-      </block>
-    </value>
-  </block>
-  <block type="operation_par"> </block>
-  <block type="defaut_sur"> </block>
-  <block type="deficit"> </block>
-</category>
-
-<category name="Contexts">
-  <block type="territoire_domain" deletable="false" editable="false">
-    <statement name="territory_cases">
-      <block type="territory_cases">
-        <field name="territory_cases">France</field>
-      </block>
-    </statement>
-  </block>
-  <block type="support_domain" deletable="false" editable="false">
-    <statement name="support_cases">
-      <block type="support_cases">
-      </block>
-    </statement>
-  </block>
-  <block type="secteur_domain" deletable="false" editable="false">
-    <statement name="secteur_cases">
-      <block type="secteur_cases">
-      </block>
-    </statement>
-  </block>
-  <block type="support_cases"></block>
-  <block type="secteur_cases"></block>
-  <block type="territory_cases">
-    <field name="territory_cases">France</field>
-  </block>
-</category>
-
-<category name="Corps">
-  <block type="avant">
-    <value name="event_name">
-      <block type="event" movable="false">
-        <field name="TEXT">dssd</field>
-      </block>
-    </value>
-    <statement name="avant_body">
-      <block type="quotepart"></block>
-    </statement>
-  </block>
-  <block type="apres">
-    <value name="event_name">
-      <block type="event" movable="false">
-        <field name="TEXT">dssd</field>
-      </block>
-    </value>
-    <statement name="apres_body">
-      <block type="quotepart"></block>
-    </statement>
-  </block>
-  <block type="quotepart"></block>
-  <block type="bonus"></block>
-  <block type="event" > </block>
- <block type="retrocession"> </block>
-
-</category>
-
-<category name="Entrées ">
-  <block type="partenaire"></block>
-  <block type="partenaire_label">
-    <value name="label">
-      <block type="label" movable= false></block>
-    </value>
-  </block>
-  <block type="dest_pool"></block>
-  <block type="dest_pool_context"></block>
-  <block type="pool_local_decl"></block>
-  <block type="entree"></block>
-  <block type="entree_value"></block>
-  <block type="constant"></block>
-  <block type="monetary"></block>
-  <block type="number"></block>
-</category>
-
-<category name="Logic">
-  <block type="quand"></block>
-  <block type="quand_statement"></block>
-  <block type="logic_compare"></block>
-  <block type="logic_operation"></block>
-  <block type="logic_negate"></block>
-</category>
-
-<category name="Misc">
-
-  <block type="math_arithmetic">
-    <field name="OP">ADD</field>
-    <value name="A">
-      <shadow type="math_number">
-        <field name="NUM">1</field>
-      </shadow>
-    </value>
-    <value name="B">
-      <shadow type="math_number">
-        <field name="NUM">1</field>
-      </shadow>
-    </value>
-  </block>
-  <block type="text">
-    <field name="TEXT"></field>
-  </block>
-</category>
-
-
-<category name="Math">
-  <block type="math_arithmetic"></block>
-
-</category>
-
-</xml>`;
+const toolbox = {
+  kind: "categoryToolbox",
+  contents: [
+    {
+      kind: "category",
+      name: "Operation",
+      contents: [
+        {
+          kind: "block",
+          type: "operation",
+          inputs: {
+            POUR: {
+              shadow: {
+                type: "territoire_domain",
+              },
+            },
+          },
+          fields: {
+            sur_assiette: "les_productions_du_chameau",
+          },
+          values: {
+            operation: {
+              block: {
+                type: "text",
+                fields: {
+                  TEXT: "",
+                },
+              },
+            },
+          },
+        },
+        {
+          kind: "block",
+          type: "operation_local_pool_decl",
+        },
+        {
+          kind: "block",
+          type: "evenement_atteint",
+          values: {
+            evenement_atteint: {
+              block: {
+                type: "quand",
+              },
+            },
+          },
+        },
+        {
+          kind: "block",
+          type: "operation_par",
+        },
+        {
+          kind: "block",
+          type: "defaut_sur",
+        },
+        {
+          kind: "block",
+          type: "deficit",
+        },
+      ],
+    },
+    {
+      kind: "category",
+      name: "Contexts",
+      contents: [
+        {
+          kind: "block",
+          type: "territoire_domain",
+          deletable: true,
+          editable: true,
+          statements: {
+            territory_cases: {
+              block: {
+                type: "territory_cases",
+                fields: {
+                  territory_cases: "France",
+                },
+              },
+            },
+          },
+        },
+        {
+          kind: "block",
+          type: "support_domain",
+          deletable: true,
+          editable: true,
+          statements: {
+            support_cases: {
+              block: {
+                type: "support_cases",
+              },
+            },
+          },
+        },
+        {
+          kind: "block",
+          type: "secteur_domain",
+          deletable: true,
+          editable: true,
+          statements: {
+            secteur_cases: {
+              block: {
+                type: "secteur_cases",
+              },
+            },
+          },
+        },
+        {
+          kind: "block",
+          type: "support_cases",
+        },
+        {
+          kind: "block",
+          type: "secteur_cases",
+        },
+        {
+          kind: "block",
+          type: "territory_cases",
+          fields: {
+            territory_cases: "France",
+          },
+        },
+      ],
+    },
+    {
+      kind: "category",
+      name: "Corps",
+      contents: [
+        {
+          kind: "block",
+          type: "avant",
+          values: {
+            event_name: {
+              block: {
+                type: "event",
+                movable: true,
+                fields: {
+                  TEXT: "dssd",
+                },
+              },
+            },
+          },
+          statements: {
+            avant_body: {
+              block: {
+                type: "quotepart",
+              },
+            },
+          },
+        },
+        {
+          kind: "block",
+          type: "apres",
+          values: {
+            event_name: {
+              block: {
+                type: "event",
+                movable: true,
+                fields: {
+                  TEXT: "dssd",
+                },
+              },
+            },
+          },
+          statements: {
+            apres_body: {
+              block: {
+                type: "quotepart",
+              },
+            },
+          },
+        },
+        {
+          kind: "block",
+          type: "quotepart",
+        },
+        {
+          kind: "block",
+          type: "bonus",
+        },
+        {
+          kind: "block",
+          type: "event",
+        },
+        {
+          kind: "block",
+          type: "retrocession",
+        },
+      ],
+    },
+    {
+      kind: "category",
+      name: "Entrées",
+      contents: [
+        {
+          kind: "block",
+          type: "partenaire",
+        },
+        {
+          kind: "block",
+          type: "partenaire_label",
+          values: {
+            label: {
+              block: {
+                type: "label",
+                movable: false,
+              },
+            },
+          },
+        },
+        {
+          kind: "block",
+          type: "dest_pool",
+        },
+        {
+          kind: "block",
+          type: "dest_pool_context",
+        },
+        {
+          kind: "block",
+          type: "pool_local_decl",
+        },
+        {
+          kind: "block",
+          type: "entree",
+        },
+        {
+          kind: "block",
+          type: "entree_value",
+        },
+        {
+          kind: "block",
+          type: "constant",
+        },
+        {
+          kind: "block",
+          type: "monetary",
+        },
+        {
+          kind: "block",
+          type: "number",
+        },
+      ],
+    },
+    {
+      kind: "category",
+      name: "Logic",
+      contents: [
+        {
+          kind: "block",
+          type: "quand",
+        },
+        {
+          kind: "block",
+          type: "quand_statement",
+        },
+        {
+          kind: "block",
+          type: "logic_compare",
+        },
+        {
+          kind: "block",
+          type: "logic_operation",
+        },
+        {
+          kind: "block",
+          type: "logic_negate",
+        },
+      ],
+    },
+    {
+      kind: "category",
+      name: "Misc",
+      contents: [
+        {
+          kind: "block",
+          type: "math_arithmetic",
+          fields: {
+            OP: "ADD",
+          },
+          values: {
+            A: {
+              block: {
+                type: "math_number",
+                fields: {
+                  NUM: 1,
+                },
+              },
+            },
+            B: {
+              block: {
+                type: "math_number",
+                fields: {
+                  NUM: 1,
+                },
+              },
+            },
+          },
+        },
+        {
+          kind: "block",
+          type: "text",
+          fields: {
+            TEXT: "",
+          },
+        },
+      ],
+    },
+    {
+      kind: "sep",
+    },
+  ],
+};
 
 const ws = Blockly.inject(blocklyDiv, { toolbox });
