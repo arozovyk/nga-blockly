af0e95f3385b4e6fc77c6c4f1420be27d9f0da8d
arozovyk <artemiy.rozovyk@ocamlpro.com>
2023-11-09 17:31:16 +0100

    save

--- src/index.js
+++ src/index.js
@@ -76,8 +76,8 @@ const runCode = () => {
   outputDiv.innerHTML = "";
 };
 
-/* load(ws);
- */runCode();
+load(ws);
+runCode();
 
 ws.addChangeListener((e) => {
   // Don't run the code when the workspace finishes loading; we're
@@ -98,5 +98,5 @@ ws.addChangeListener((e) => {
   // UI events are things like scrolling, zooming, etc.
   // No need to save after one of these.
   if (e.isUiEvent) return;
-/*   save(ws);
- */});
+  save(ws);
+});
